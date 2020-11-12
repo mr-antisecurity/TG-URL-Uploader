15159b15b1b33d59ec69e0c5233be5b98d4ccdfb
mr-antisecurity <73610922+mr-antisecurity@users.noreply.github.com>
2020-11-12 15:24:09 +0500

    Update

--- sample_config.py
+++ sample_config.py
@@ -20,8 +20,8 @@ class Config(object):
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    # CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    ChUNK_SIZE = 128
+    #CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    CHUNK_SIZE = 128
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # proxy for accessing youtube-dl in GeoRestricted Areas
